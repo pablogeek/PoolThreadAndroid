24224ddc7ed3cc7133a5a3967d0c15437dabfa45
Pablo <pablito.software@gmail.com>
2013-05-22 09:52:32 +0200

    Update MainActivity.java

--- ThreadPoolAndroid/src/com/example/threadpoolandroid/MainActivity.java
+++ ThreadPoolAndroid/src/com/example/threadpoolandroid/MainActivity.java
@@ -1,4 +1,3 @@
-
 package com.example.threadpoolandroid;
 
 import com.example.threadpoolandroid.taskmanager.TransactionTask;
@@ -16,6 +15,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        TaskManager.getInstance().downloadTransactions(handlerUpdateUI);
     }
 
     @Override
